841c3b6b39a3ed82b0dfc4f4393c52ddb2c2427e
Jason Wynne <jwynne@martinwilliams.com>
2016-09-03 09:11:22 -0500

    nav added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+$(function() {
+    	
+	var $win = $(window),
+			winWidth = $win.width(),
+			winHeight = $win.height();
 
+	console.log(winWidth+","+winHeight);
+
+	$('.menu-icon').click(function(){
+		$('nav').toggleClass('open');
+	});
+
+
+
+
+	// on resize events 
+		$win.resize(function() {
+			winWidth = $win.width();
+			winHeight = $win.height();
+
+		});
+});
